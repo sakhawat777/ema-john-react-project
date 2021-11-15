74ea377b37f40b243950f82747b60eb3b50773ab
sakhawat777 <biplobbms@gmail.com>
2021-11-15 19:25:36 +0600

    Review Order Added

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { getDatabaseCart } from '../../utilities/databaseManager';
-
+import fakeData from '../../fakeData';
 const Review = () => {
 	const [cart, setCart] = useState([]);
 	useEffect(() => {
 		const saveCart = getDatabaseCart();
 		const productKeys = Object.keys(saveCart);
-		const counts = Object.values(saveCart);
-		// const counts = productKeys.map(key => saveCart([key]));
+		// const counts = Object.values(saveCart);
+		const cartProducts = productKeys.map((key) => {
+			const product = fakeData.find((pd) => pd.key === key);
+			product.quantity = saveCart[key];
+			return product;
+		});
 		console.log(productKeys);
-		console.log(counts);
-		console.log(saveCart);
+		console.log(cartProducts);
+		setCart(cartProducts);
 	});
 	return (
 		<div>
-			<h1>Review is coming soon</h1>
+			<h1>Review Items: {cart.length}</h1>
 		</div>
 	);
 };
