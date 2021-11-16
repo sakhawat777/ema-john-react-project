d007e615e20f503313b39fab1e27b1778d06ef77
sakhawat777 <biplobbms@gmail.com>
2021-11-16 09:40:29 +0600

    Remove Cart Added

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import {
+	getDatabaseCart,
+	removeFromDatabaseCart,
+} from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
 import ReviewItem from './ReviewItem/ReviewItem';
 const Review = () => {
 	const [cart, setCart] = useState([]);
+	const handleRemoveItem = (productKey) => {
+		// console.log('Remove Clicked', productKey);
+		const newCart = cart.filter((pd) => pd.key !== productKey);
+		setCart(newCart);
+		removeFromDatabaseCart(productKey);
+	};
 	useEffect(() => {
 		const saveCart = getDatabaseCart();
 		const productKeys = Object.keys(saveCart);
@@ -21,7 +30,10 @@ const Review = () => {
 		<div>
 			<h1>Review Items: {cart.length}</h1>
 			{cart.map((pd) => (
-				<ReviewItem product={pd} key={pd.key}></ReviewItem>
+				<ReviewItem
+					product={pd}
+					handleRemoveItem={handleRemoveItem}
+					key={pd.key}></ReviewItem>
 			))}
 		</div>
 	);
--- src/components/Review/ReviewItem/ReviewItem.js
+++ src/components/Review/ReviewItem/ReviewItem.js
@@ -2,14 +2,18 @@ import React from 'react';
 import '../ReviewItem/ReviewItem.css';
 const ReviewItem = (props) => {
 	console.log(props);
-	const { name, quantity } = props.product;
+	const { name, quantity, key } = props.product;
 
 	return (
 		<div className='review-item'>
 			<h1 className='product-name'>{name}</h1>
 			<p>Quantity: {quantity}</p>
 			<br />
-			<button className='add-cart-btn'>Remove</button>
+			<button
+				className='add-cart-btn'
+				onClick={() => props.handleRemoveItem(key)}>
+				Remove
+			</button>
 		</div>
 	);
 };
